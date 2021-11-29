c6100cd19c2186666caa3197608731ee23894583
daffamf <daffamuhammadfirdaus5@gmail.com>
2021-11-29 22:48:14 +0700

    UI carousel

---
+++ src/pages/carousel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Carousel from '../components/Carousel/carousel'
+
+function carousel() {
+    return (
+        <div>
+            <Carousel />
+        </div>
+    )
+}
+
+export default carousel
