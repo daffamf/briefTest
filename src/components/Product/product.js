13d073174822b6706c1a57319e1b226b8d32d38d
daffamf <daffamuhammadfirdaus5@gmail.com>
2021-12-02 10:59:56 +0700

    product api

--- src/components/Product/product.js
+++ src/components/Product/product.js
@@ -8,26 +8,27 @@ const Product = ({ items }) => {
     const [visable, setVisable] = useState(3)
 
     const showMoreItems = () => {
-        setVisable((prevValue) => prevValue + 3)
+        setVisable((visable) => visable + 3)
     }
 
 
     console.log('ddd', items.data)
-    
+
 
     return (
         <section className='py-4'>
-            
+
             <h1 className='container'> Product List</h1>
             <hr />
-            {/* {items.data.filter(item => ( */}
+            {/* {items.map(item => { */}
 
-                <div className='row justify-content-center' key={''}>
+                <div className='row justify-content-center' >
                     <div className="col-11 col-md-6 col-lg-3 nx-0 mb-4" >
                         <div className='card p-0 overflow-hidden h-100 shadow'>
-                            <img src={`https://assets.tokodistributor.com/product/8ca84f7ced38aba69595f4516bb89284`} className='card-img-top' />
+                            <img src={'https://assets.tokodistributor.com/product/95e262e4bede7c7548f8a2c19f1ecefa.jpg'} className='card-img-top' />
                             <div className="card-body">
                                 <h5 className='card-tittle'>{'BANDUNG'}</h5>
+                                <h2 className='card-tittle' aria-disabled>{'sepatu'}</h2>
                                 <p className='Card-text'>{'Rp.4200000'}</p>
                             </div>
 
@@ -36,12 +37,12 @@ const Product = ({ items }) => {
                     </div>
 
                 </div>
-                
-            {/* ))} */}
 
+        {/* })} */}
             <button className='load' onClick={showMoreItems}>
                 Lihat Lainnyah..
             </button>
+
 
             <hr />
 
